db4bcf295db9cd09c9d496e171c4c2e42e193530
serhijko <sp_by@tut.by>
2018-11-18 00:25:02 +0300

    Unidirectional Data Flow

--- src/App.js
+++ src/App.js
@@ -28,6 +28,14 @@ class App extends Component {
     this.state = {
       list: list,
     };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
   }
 
   render() {
@@ -41,6 +49,14 @@ class App extends Component {
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
           </div>
         )}
       </div>
