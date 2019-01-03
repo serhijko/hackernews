90f5fd7d417e317b3a189d774d4f7a9e186ec18a
serhijko <serhijko>
2019-01-03 18:00:56 +0300

    Component Interface with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
@@ -214,7 +215,7 @@ const Table = ({ list, onDismiss }) =>
 
 const Button = ({
   onClick,
-  className = '',
+  className,
   children,
 }) =>
   <button
@@ -224,6 +225,29 @@ const Button = ({
   >
     {children}
   </button>
+
+Button.defaultProps = {
+  className: '',
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
+
+Table.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      objectID: PropTypes.string.isRequired,
+      author: PropTypes.string,
+      url: PropTypes.string,
+      num_comments: PropTypes.number,
+      points: PropTypes.number,
+    })
+  ).isRequired,
+  onDismiss: PropTypes.func.isRequired,
+};
 
 export default App;
 
